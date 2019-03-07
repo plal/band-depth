aaaf8b118780371d924a88dcd6e706bc540e7d7b
plal <plal@cin.ufpe.br>
2019-03-07 20:04:51 -0300

    fix typo

--- band-depth.c
+++ band-depth.c
@@ -49,26 +49,26 @@ f64 get_number_with_prob(f64 prob, f64 val1, f64 val2) {
 // [ Curve ... values ... ]
 //
 typedef struct {
-	s32    num_pos32s; // num pos32s
+	s32    num_points; // num pos32s
 	s32    num_bytes;
 	f64 depth;
 	f64 values[];
 } Curve;
 
-s32 curve_num_bytes(s32 num_pos32s)
+s32 curve_num_bytes(s32 num_points)
 {
-	return sizeof(Curve) + num_pos32s * sizeof(f64);
+	return sizeof(Curve) + num_points * sizeof(f64);
 }
 
-Curve* curve_new(s32 num_pos32s)
+Curve* curve_new(s32 num_points)
 {
-	s32 num_bytes = curve_num_bytes(num_pos32s);
+	s32 num_bytes = curve_num_bytes(num_points);
 	Curve *result = malloc(num_bytes);
 	result[0] = (Curve) {
-		.num_pos32s = num_pos32s,
+		.num_points = num_points,
 		.num_bytes  = num_bytes,
 	};
-	for (s32 i=0;i<num_pos32s;++i) {
+	for (s32 i=0;i<num_points;++i) {
 		result->values[i] = 0.0;
 	}
 	return result;
@@ -86,20 +86,20 @@ void curve_free(Curve *curve)
 	free(curve);
 }
 
-Curve* curve_new_constant(s32 num_pos32s, f64 value) {
-	Curve *curve = curve_new(num_pos32s);
-	for (s32 i=0;i<num_pos32s;++i) {
+Curve* curve_new_constant(s32 num_points, f64 value) {
+	Curve *curve = curve_new(num_points);
+	for (s32 i=0;i<num_points;++i) {
 		curve->values[i] = value;
 	}
 	return curve;
 }
 
-Curve* curve_generate(s32 num_pos32s) {
-	Curve *curve = curve_new(num_pos32s);
+Curve* curve_generate(s32 num_points) {
+	Curve *curve = curve_new(num_points);
 	f64 c     = get_number_with_prob(0.4, 0,  1);
 	f64 sigma = get_number_with_prob(0.5, 1, -1);
 
-	for(s32 i=0;i<num_pos32s;i++) {
+	for(s32 i=0;i<num_points;i++) {
 		f64 t = (f64)rand() / (f64)RAND_MAX;
 		if(t >= 0.3) {
 			curve->values[i] = (i + (c*sigma*S));
@@ -111,7 +111,7 @@ Curve* curve_generate(s32 num_pos32s) {
 }
 
 s32 is_curve_between(Curve *curve1, Curve *curve2, Curve *curve3) {
-	s32 size = curve1->num_pos32s;
+	s32 size = curve1->num_points;
 	for(s32 i=0;i<size;++i) {
 		f64 a = curve1->values[i];
 		f64 b = Min(curve2->values[i],curve3->values[i]);
@@ -124,7 +124,7 @@ s32 is_curve_between(Curve *curve1, Curve *curve2, Curve *curve3) {
 }
 
 void print_curve(Curve *curve) {
-	s32 size = curve->num_pos32s;
+	s32 size = curve->num_points;
 	for(s32 i=0; i < size; i++) {
 		printf("curve[%d]:%f\n",i,curve->values[i]);
 	}
