f569ad1a80c83aea4630c9fb13a1a4f956b6a86b
Lauro Lins <lauro.lins@gmail.com>
2020-06-26 11:16:01 -0400

    update

--- web/band_depth.js
+++ web/band_depth.js
@@ -1,9 +1,6 @@
-/*
-*/
-
-
-const MSEC_PER_FRAME = 33
-
+//------------------------------------------------------------------------------
+// UTIL
+//------------------------------------------------------------------------------
 function u_parse_tables(st, field_delim, record_delim, table_delim)
 {
 	let i = 0
@@ -87,6 +84,23 @@ function u_parse_tables(st, field_delim, record_delim, table_delim)
 	return results
 }
 
+
+//------------------------------------------------------------------------------
+// MAIN
+//------------------------------------------------------------------------------
+
+const MSEC_PER_FRAME = 33
+
+		//depth values order: original, fast, tdigest, original modified, fast modified, tdigest modified, sliding (wsize 15), extremal
+const DEPTH_ORIGINAL = 0
+const DEPTH_FAST = 1
+const DEPTH_TDIGEST = 2
+const DEPTH_ORIGINAL_MODIFIED = 3
+const DEPTH_FAST_MODIFIED = 4
+const DEPTH_TDIGEST_MODIFIED = 5
+const DEPTH_SLIDING_15 = 6
+const DEPTH_EXTREMAL = 7
+
 function InconsistentSizeException(message) {
 	this.message = message;
  	this.name = "InconsistentSizeException";
@@ -104,16 +118,17 @@ function merge_tables(inputs, outputs) {
 	let data = []
 
 	for(let i=0; i<rows; i++) {
-		let tseries = {values:[], depths:[]}
+
+		let tseries = {values:[], depths: []}
 
 		for(let j=0; j<in_cols;j++) {
-			tseries.values.push(inputs.column_values[j][i])
+			tseries.values.push(parseFloat(inputs.column_values[j][i]))
 		}
 
 		//depth values order: original, fast, tdigest, original modified, fast modified, tdigest modified, sliding (wsize 15), extremal
 		for(let j=0; j<out_cols;j=j+2) {
 			//console.log(j)
-			tseries.depths.push(outputs.column_values[j][i]);
+			tseries.depths.push(parseFloat(outputs.column_values[j][i]));
 		}
 		data.push(tseries)
 	}
@@ -122,8 +137,8 @@ function merge_tables(inputs, outputs) {
 }
 
 
-global = { ui: {} }
-data   = {}
+global = { ui: {}, mouse: { position:[0,0], last_position:[0,0] }, events: [] }
+data   = { records: [], focused_timestep: null, focused_record: null }
 
 function event_loop()
 {
@@ -154,9 +169,22 @@ function prepare_ui()
 	body.style.margin='0px'
 	body.appendChild(main_div)
 	// main_div.appendChild(main_canvas)
+
+
+	global.ui.main_div.onmousemove = function(e){
+		global.events.push(e)
+	}
+
+	global.ui.main_div.onmouseup= function(e){
+		global.events.push(e)
+	}
+
+	global.ui.main_div.onmousedown = function(e){
+		global.events.push(e)
+	}
 }
 
-function update_main_canvas()
+function update_timeseries_canvas()
 {
 	let canvas = global.ui.main_canvas
 	let ctx = canvas.getContext('2d')
@@ -181,14 +209,18 @@ function update_main_canvas()
 	// console.log('version updated: '+global.version)
 	ctx.clearRect(0,0,canvas.width, canvas.height)
 
+	let num_records   = data.records.length	
+	let num_timesteps = data.records[0].values.length
+
 	// find ranges of the time series
 	let x_min_value = 0
-	let x_max_value = data.input.columns-1
-	let y_min_value = data.input.column_values[0][0]
+	let x_max_value = num_timesteps-1
+	let y_min_value = data.records[0].values[0]
 	let y_max_value = y_min_value
-	for (let i=0;i<data.input.rows;i++) {
-		for (let j=0;j<data.input.columns;j++) {
-			let v = parseFloat(data.input.column_values[j][i])
+	for (let i=0;i<num_records;i++) {
+		let values = data.records[i].values
+		for (let j=0;j<num_timesteps;j++) {
+			let v = values[j]
 			y_min_value = Math.min(v, y_min_value)
 			y_max_value = Math.max(v, y_max_value)
 		}
@@ -199,29 +231,119 @@ function update_main_canvas()
 
 	function map(x, y) {
 		let px = tseries_rect[0] + (1.0 * (x - x_min_value) / (x_max_value - x_min_value)) * tseries_rect[2]
-		let py = tseries_rect[1] + (1.0 * (y - y_min_value) / (y_max_value - y_min_value)) * tseries_rect[3]
-		py = tseries_rect[3] - 1 - py
+		let py = tseries_rect[1] + (tseries_rect[3] - 1 - (1.0 * (y - y_min_value) / (y_max_value - y_min_value)) * tseries_rect[3])
 		return [px,py]
 	}
 
-	for (let i=0;i<data.input.rows;i++) {
+	let closest_timestep = null
+	let closest_record  = null
+	let min_distance_threshold = 5 * 5
+	let closest_distance = 100000
+	function update_closest_point(record, timestep, px, py) {
+		let dx = global.mouse.position[0] - px
+		let dy = global.mouse.position[1] - py
+		let dist = dx * dx + dy * dy
+		if (dist <= min_distance_threshold && dist < closest_distance) {
+			closest_record = record
+			closest_timestep = timestep
+		}
+	}
+
+	//
+	// render timeseries
+	//
+
+	function draw_timeseries(i) {
+		let values = data.records[i].values
 		ctx.beginPath()
-		let y0 = parseFloat(data.input.column_values[0][i])
+		let y0 = values[0]
 		let p = map(0,y0)
+		update_closest_point(i, 0, p[0], p[1])
 		ctx.moveTo(p[0],p[1])
-		for (let j=1;j<data.input.columns;j++) {
-			let yi = parseFloat(data.input.column_values[j][i])
+		for (let j=1;j<num_timesteps;j++) {
+			let yi = values[j]
 			p = map(j,yi)
+			update_closest_point(i, j, p[0], p[1])
 			ctx.lineTo(p[0],p[1])
 		}
 		ctx.stroke()
 	}
+
+	function draw_point(i, j, r) {
+		let values = data.records[i].values
+		let yi = values[j]
+		p = map(j,yi)
+		update_closest_point(i, j, p[0], p[1])
+		ctx.beginPath()
+		ctx.arc(p[0],p[1],r,0,Math.PI*2,true)
+		ctx.stroke()
+	}
+
+	ctx.strokeStyle = '#0000007f'
+	ctx.lineWidth  = 1
+	for (let i=0;i<num_records;i++) {
+		if (data.focused_record == null || (i != data.focused_record)) {
+			draw_timeseries(i)
+		}
+	}
+
+	if (data.focused_record != null) {
+		ctx.strokeStyle = '#8800007f'
+		ctx.lineWidth  = 3
+		draw_timeseries(data.focused_record)
+
+		ctx.strokeStyle = '#880000ff'
+		draw_point(data.focused_record, data.focused_timestep, 3)
+
+		// print the details of the focused timeseries
+		let record = data.records[data.focused_record]
+		let value = record.values[data.focused_timestep]
+		let date = new Date(new Date(2018,0,1).getTime() + (1000 * 24 * 60 * 60 * data.focused_record))
+		let text = `ID: ${data.focused_record} ${date.toDateString()} ${data.focused_timestep}h  #trips: ${value}  ED: ${record.depths[DEPTH_EXTREMAL]}`
+		ctx.font = '24px Monospace';
+		ctx.textAlign = 'right';
+		ctx.fillText(text, rect[0] + rect[2] - 10, rect[1] + rect[3] - 12);
+	}
+
+	// update focused record
+	data.focused_record   = closest_record
+	data.focused_timestep = closest_timestep
 }
 
+function process_events()
+{
+	for (let i=0;i<global.events.length;++i) {
+		let e = global.events[i]
+		console.log(e.type)
+
+		switch(e.type) {
+			case "mousemove": {
+				global.mouse.position      = [e.x, e.y]
+				global.mouse.last_position = global.mouse.position
+				console.log(e.type)
+			} break
+			case "mousedown": {
+				console.log(e.type)
+			} break
+			case "mouseup": {
+				console.log(e.type)
+			} break
+		}
+
+		// if (e == 
+		// // console.log('update from ' + global.mouse.current + ' to ' + [e.x, e.y])
+		// global.mouse.position      = [e.x, e.y]
+		// global.mouse.last_position = global.mouse.position
+	}
+	global.events.length = 0
+}
 
 function update()
 {
-	update_main_canvas()
+	// event processing
+	process_events()
+
+	update_timeseries_canvas()
 
 	setTimeout(update, MSEC_PER_FRAME)
 }
@@ -229,14 +351,12 @@ function update()
 function main()
 {
 	// read the two tables
-	data.input  = u_parse_tables(data_input, '|', '\n', String.fromCharCode(1))[0]
-	data.output = u_parse_tables(data_output, '|', '\n', String.fromCharCode(1))[0]
-	//console.log(data.input)
-	//console.log(data.output)
-	full_table = merge_tables(data.input, data.output)
-	console.log(full_table)
+	tmp_input  = u_parse_tables(data_input, '|', '\n', String.fromCharCode(1))[0]
+	tmp_output = u_parse_tables(data_output, '|', '\n', String.fromCharCode(1))[0]
+	data.records =  merge_tables(tmp_input, tmp_output)
 
 	prepare_ui()
 
 	setTimeout(update, MSEC_PER_FRAME)
 }
+
