c2af0e41b470faed9aade7c72a2081e5728d1764
plal <lins.pedro17@gmail.com>
2021-06-28 20:38:10 -0300

    adds nba data scripts

---
+++ stocks/data/nba_players_data.py
@@ -0,0 +1,26 @@
+from basketball_reference_web_scraper import client
+from basketball_reference_web_scraper.data import OutputType
+
+import pandas as pd
+
+days   = range(1,32)
+months = [1,2,3,4,5,6,9,10,11,12]
+years  = [2015,2016,2017,2018,2019,2020]
+
+def format_number(number):
+    if len(str(number)) == 1:
+        return "0"+str(number)
+    return str(number)
+
+for year in years:
+    for month in months:
+        for day in days:
+            date = str(year)+'-'+format_number(month)+'-'+format_number(day)
+            data = client.player_box_scores(day=day,month=month,year=year)
+            if data != []:
+                for player in data:
+                    player["date"] = date
+
+                df = pd.DataFrame(data)
+                fname = "nba/nba_player_stats_"+date+'.csv'
+                df.to_csv(fname,sep='|',index=False)
